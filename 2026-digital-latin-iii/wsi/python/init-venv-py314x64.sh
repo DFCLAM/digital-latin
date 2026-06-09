c425114f03a33eaf0b75022b1babbb206f32f1fe
Giulio Quaresima <giulio.quaresima@unisi.it>
2026-06-09 14:43:32 +0200

    venv for py 3.14

---
+++ 2026-digital-latin-iii/wsi/python/init-venv-py314x64.sh
@@ -0,0 +1,3 @@
+python3.14 -m venv venv-py314x64
+. venv-py314x64/bin/activate
+python -m pip install --upgrade pip
--- 2026-digital-latin-iii/wsi/python/venv-py3x64.sh
+++
@@ -1,3 +0,0 @@
-python3 -m venv venv-py3x64
-. venv-py3x64/bin/activate
-python -m pip install --upgrade pip
